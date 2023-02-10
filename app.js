6864c10f0c8ebcb1b28d727f7bc0adee7dfe2bc0
salo-rodriguezz <123768442+salo-rodriguezz@users.noreply.github.com>
2023-02-10 14:02:16 -0500

    imagenes java script

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
-document.addEventListener("DOMContentLoaded", () => {});
+document.addEventListener("DOMContentLoaded", () => {
+  const cardAdj = [
+    {
+      neme: "s",
+      img: "imagenes/s.png"
+    },
+    {
+      neme: "a",
+      img: "imagenes/a.png"
+    },
+    {
+      neme: "l",
+      img: "imagenes/l.png"
+    },
+    {
+      neme: "o",
+      img: "imagenes/o.png"
+    },
+    {
+      neme: "m",
+      img: "imagenes/m.png"
+    },
+    {
+      neme: "e",
+      img: "imagenes/e.png"
+    }
+  ];
+});
